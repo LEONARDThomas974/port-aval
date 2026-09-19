a0af34168b6294a02764d38afea0ba1d5a19cab9
LEONARD Thomas <52138747+LEONARDThomas974@users.noreply.github.com>
2026-09-19 12:16:18 +0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,20 @@
+import 'package:flutter/material.dart';
 
+import 'ui/ecran_partie.dart';
+import 'ui/theme.dart';
+
+void main() => runApp(const AppPortAval());
+
+class AppPortAval extends StatelessWidget {
+  const AppPortAval({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Port-Aval',
+      debugShowCheckedModeBanner: false,
+      theme: themePortAval(),
+      home: const EcranPartie(),
+    );
+  }
+}
